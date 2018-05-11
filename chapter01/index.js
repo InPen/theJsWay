9e6487ff55bd2df9b6201d2159f9719e701d2b9e
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-05-11 12:34:42 -0400

    Add program that displays the results of adding, subtracting, multiplying and dividing 6 by 3

--- chapter01/index.js
+++ chapter01/index.js
@@ -1,2 +1,34 @@
+// program that displays your name and age
 console.log("Maria Inés")
 console.log(27)
+
+// program that displays the results of adding,
+// subtracting, multiplying and dividing 6 by 3
+
+// Addition
+function add(a, b) {
+  return a + b
+}
+const addSolution = add(6,3)
+console.log('6 + 3 = ' + addSolution)
+
+// Subtraction
+function subtract(pizza, macAndCheesels) {
+  return pizza - macAndCheesels
+}
+const subtractSolution = subtract(6,3)
+console.log('6 - 3 = ' + subtractSolution)
+
+// Multiplication
+function multiply(j, i) {
+  return j * i
+}
+multiply(6,3)
+console.log('6 * 3 = ' + multiply(6,3))
+
+// Division
+function divide(pb, jay) {
+  return pb/jay
+}
+divide(6, 3)
+console.log('6 / 3 = ' + divide(6,3))
