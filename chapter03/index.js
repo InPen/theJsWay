4c37f49a780f501ca3cd1a31914b086b40193b3a
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-05-15 14:32:35 -0400

    Begin chapter03

---
+++ chapter03/index.js
@@ -0,0 +1,6 @@
+
+// Write a program that accepts a day name from the user
+const dayName = prompt('What day is it today?')
+alert('Thank Glob is ' + dayName)
+// then shows the name of the following day.
+// Incorrect inputs must be taken into account.
